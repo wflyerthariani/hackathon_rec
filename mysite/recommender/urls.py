e5e0f81e3c1346189076571419728ba8f3546127
Arman Thariani <wflyerthariani@gmail.com>
2022-03-10 22:06:43 -0800

    Initial database information added

--- mysite/recommender/urls.py
+++ mysite/recommender/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('loaddata/', views.load_data, name='loader')
 ]
--- mysite/recommender/views.py
+++ mysite/recommender/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import csv
+import os
+from django.conf import settings
+from recommender.models import Item
 
 def index(request):
     context = {}
     return render(request, 'recommender/index.html', context)
+
+def load_data(request):
+    with open(os.path.join(settings.BASE_DIR, 'DatafinitiElectronicsProductData.csv')) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count >= 1:
+                if len(Item.objects.filter(item_name = row[13])) == 0:
+                    Item.objects.create(item_name = row[13], item_image = row[9])
+            line_count += 1
+        print(f'Processed {line_count} lines.')
+    return redirect(index)
