951e9bd7542c63c6d7b8d491c2e39149e6e86337
Lochy <lochy.rode@gmail.com>
2022-03-10 16:50:45 -0800

    Changed Hello World message

--- mysite/recommender/views.py
+++ mysite/recommender/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the recommender index.")
+    return HttpResponse("Hello, world. This be Lanklan. You're at the recommender index.")
