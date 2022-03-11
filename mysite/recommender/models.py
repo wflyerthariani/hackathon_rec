b2e4b8a01bdb21e970b33fe76d080278d9c445fe
Arman Thariani <wflyerthariani@gmail.com>
2022-03-10 18:16:45 -0800

    Created database

--- mysite/recommender/models.py
+++ mysite/recommender/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    item_name = models.CharField(max_length=200)
+    item_image = models.CharField(max_length=200)
+    
+class Store(models.Model):
+    store_name = models.CharField(max_length=200)
+    store_items = models.ManyToManyField(Item)
+
+class Review(models.Model):
+    review_item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    review_text = models.CharField(max_length=10000)
