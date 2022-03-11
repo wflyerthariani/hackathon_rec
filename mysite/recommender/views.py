6f26fafd5b7d513995b8fc96ff4afaa874e554c5
Arman Thariani <wflyerthariani@gmail.com>
2022-03-10 17:23:36 -0800

    Fixed issue generated by template not found

--- mysite/recommender/views.py
+++ mysite/recommender/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    template = loader.get_template('templates/recommender/index.html')
     context = {}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'recommender/index.html', context)
